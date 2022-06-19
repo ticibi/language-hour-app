7407d97d21954ce68821e6828570468a747ca1a0
ticibi <tcb003@outlook.com>
2022-06-19 11:43:41 -0400

    update

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ LST = st.secrets["LST_SHEET_ID"]
 USER_SHEET = st.secrets["USER_SHEET"]
 USER_SHEET_ID = st.secrets["USER_SHEET_ID"]
 SERVICE_INFO = st.secrets["google_service_account"]
+PASSWORD = st.secrets["PASSWORD"]
 
 st.set_page_config(page_title="Language Hour Entry", page_icon="🌐", layout="centered")
 credentials = service_account.Credentials.from_service_account_info(info=SERVICE_INFO, scopes=SCOPES)
@@ -54,15 +55,15 @@ def sum_hours(name):
     hours = get_data(column="Hours", sheet=name, worksheet=LHT)
     return sum([float(hr) for hr in hours])
 
-hashed_passwords = stauth.Hasher(get_data(column="Password", sheet="Members", worksheet=LHT)).generate()
-authenticator = stauth.Authenticate(get_data(column="Name", sheet="Members", worksheet=LHT), get_data(column="Username", sheet="Members", worksheet=LHT), hashed_passwords, "lht_cookie", "lht", cookie_expiry_days=30)
+user_data = get_data(column=None, sheet="Members", worksheet=LHT)
+hashed_passwords = stauth.Hasher([PASSWORD for _ in user_data]).generate()
+authenticator = stauth.Authenticate(user_data["Name"].tolist(), user_data["Username"].tolist(), hashed_passwords, "lht_cookie", "lht", cookie_expiry_days=30)
 Name, authentication_status, username = authenticator.login("Language Hour Tracker Login", "main")
 
-
-def main():
+if authentication_status:
     st.title("Language Hour Entry")
     data = get_data(column=None, sheet=Name, worksheet=LHT)
-    with st.form(key="user_form", clear_on_submit=True) as form:
+    with st.form(key="user_form") as form:
         cols = st.columns((2, 1, 1))
         name = cols[0].text_input(label="Name", value=Name, placeholder="Last name", disabled=True)
         hours = cols[1].text_input(label=f"Hours - {sum_hours(name)} submitted")
@@ -72,33 +73,29 @@ def main():
         speaking = cols[0].checkbox(label="Speaking")
         cols = st.columns((2, 1))
         description = cols[0].text_area(label="Description", height=150, placeholder="describe what you did/understood/struggled with\nexample:\nlistened to lvl 2+ passages about politics in Lebanon etc...\nthen answered questions about it and scored 90%. etc")
-        vocab = cols[1].text_area(label="Vocab", height=150, placeholder="list the vocab you learned\nexample:\nبطيخ - watermelon\nاحتكار - monopoly\netc")
+        vocab = cols[1].text_area(label="Vocab", height=150, placeholder="list the vocab you learned and/or reviewed\nexample:\nبطيخ - watermelon\nاحتكار - monopoly")
         cols = st.columns(2)
         uploaded_file = st.file_uploader(label="Upload your 623A")
         submitted = cols[0].form_submit_button(label="Submit")
 
-    expander = st.expander("Show my Language Hour entries")
-    with expander:
+    with st.expander("Show my Language Hour entries") as expander:
         st.dataframe(data)
 
     st.download_button(label="📥 Download my Language Hours", data=to_excel(data), file_name="myLanguageHours.xlsx")
-    
+
     if uploaded_file is not None:
         pass
 
     if submitted:
-        data = get_data(column=None, sheet=name, worksheet=LHT)
-        if len(description) < 1:
-            st.error("You need to include what you studied...")
-            return
-        if float(hours) <= 0:
-            st.error("You need to study longer than 0 hours...")
-            return
-        if not any([listening, reading, speaking]) == True:
-            st.error("You need to select at least 1 modality...")
-            return
-        
-
+        #if len(description) < 1:
+        #    st.error("You need to include what you studied...")
+        #    
+        #if float(hours) <= 0:
+        #    st.error("You need to study longer than 0 hours...")
+        #    
+        #if not any([listening, reading, speaking]) == True:
+        #    st.error("You need to select at least 1 modality...")
+            
         modality = ""
         if listening:
             modality += "L"
@@ -108,14 +105,10 @@ def main():
             modality += "S"
 
         add_entry(worksheet=LHT, sheet=name, data=[[str(date), float(hours), modality, description, ",".join(vocab.split())]])
-        st.success(f"Thanks {name}! Your entry has been submitted")
+        st.success(f"Thanks {name.split(',')[1]}! Your entry has been submitted")
         st.balloons()
-
-if __name__ == "__main__":
-    if authentication_status:
-        main()
         authenticator.logout("Logout")
-    elif authentication_status == False:
-        st.error('Username or password is incorrect')
-    elif authentication_status == None:
-        st.warning('Please enter your username and password')
+elif authentication_status == False:
+    st.error('Username or password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
