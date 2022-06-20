2a1f74a393f193af723093bf6177361401bf3185
ticibi <tcb003@outlook.com>
2022-06-20 13:37:28 -0400

    update sidebar

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from google.oauth2 import service_account
 from googleapiclient.discovery import build, MediaFileUpload
-from datetime import datetime
-
+from googleapiclient.http import MediaIoBaseDownload
 
 
 SCOPES = [
@@ -32,8 +31,39 @@ credentials = service_account.Credentials.from_service_account_info(info=SERVICE
 sheets_service = build(serviceName="sheets", version="v4", credentials=credentials)
 drive_service = build(serviceName="drive", version="v3", credentials=credentials)
 
+def create_folder(name):
+    file_metadata = {
+        "name": name,
+        "mimeType": "application/vnd.google-apps.folder",
+        "parents": [FOLDER_ID],
+    }
+    folder = drive_service.files().create(body=file_metadata, fields="id").execute()
+    return folder
 
-def format_modality(*args, **kwargs):
+def add_member(name, username, password, flags) -> None:
+    data = [name, username, password, flags]
+    # add user info to the Members sheet on LHT
+    add_entry(worksheet=LHT, sheet="Members", data=data, range="A:D")
+    body = {
+        "requests": [
+            {
+                "addSheet": {
+                    "properties": {
+                        "title": name,
+                    }
+                }
+            }
+        ]
+    }
+    # create a new tab with the users name
+    response = sheets_service.spreadsheets().batchUpdate(
+        spreadsheetId=USER_SHEET_ID,
+        body=body
+    ).execute()
+    # create folder on the drive with the users name
+    create_folder(name)
+
+def format_modality(*args, **kwargs) -> str:
     output = ""
     if kwargs["listening"]:
         output += "L"
@@ -88,15 +118,15 @@ def get_folder_id(folder_name) -> str:
     file = drive_service.files().list(q=query, fields="files(id)").execute()
     return file["files"][0]["id"]
 
-def upload_file(file, folder_name):
-        with open(f"temp/{file.name}", "wb") as f:
-            f.write(file.getbuffer())
-        file_metadata = {
-                "name": f"{file.name}",
-                "parents": [get_folder_id(folder_name)],
-            }
-        media = MediaFileUpload(f"temp/{file.name}", mimetype="*/*")
-        drive_service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+def upload_file(file, folder_name) -> None:
+    with open(f"temp/{file.name}", "wb") as f:
+        f.write(file.getbuffer())
+    file_metadata = {
+            "name": f"{file.name}",
+            "parents": [get_folder_id(folder_name)],
+        }
+    media = MediaFileUpload(f"temp/{file.name}", mimetype="*/*")
+    drive_service.files().create(body=file_metadata, media_body=media, fields="id").execute()
 
 def get_files(folder_name):
     folder_id = get_folder_id(folder_name)
@@ -109,19 +139,40 @@ def get_files(folder_name):
         files.extend(response.get("files"))
     return files
 
+def download_file(file_id, file_name):
+    request = drive_service.files().get_media(fileId=file_id)
+    bytes_data = BytesIO()
+    downloader = MediaIoBaseDownload(fd=bytes_data, request=request)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+    bytes_data.seek(0)
+    with open(os.path.join("./LanguageHourFiles", file_name), "wb") as f:
+        f.write(bytes_data.read())
+        f.close()
+
+def get_subs(name) -> list:
+    df = get_data(column=None, sheet="Main", worksheet=LST, range="A:K")
+    subs = df[["Name", "Supervisor"]].loc[df["Supervisor"] == name]
+    return list(subs["Name"])
+
 def entry_page(*args, **kwargs):
     st.title("Language Hour Entry")
     with st.sidebar:
         st.header(f"Welcome {kwargs['name']}")
-        data = get_data(column=None, sheet=kwargs["name"], worksheet=LHT)
         account_page(kwargs["name"], kwargs["username"])
         with st.expander(label="Download/Upload Files"):
+            data = get_data(column=None, sheet=kwargs["name"], worksheet=LHT)
             st.download_button(label="📥 Download My Language Hours", data=to_excel(data), file_name="myLanguageHours.xlsx")
             uploaded_file = st.file_uploader(label="Upload a 623A entry or ILTP", type=["pdf", "txt", "docx"])
         with st.expander(label="My Files"):
             files = get_files(kwargs["name"])
             for f in files:
                 st.button(label=f["name"])
+        with st.expander(label="My Troops"):
+            subs = get_subs(kwargs["name"])
+            for s in subs:
+                st.button(label=s)
 
     form = st.form(key="user_form", clear_on_submit=True)
     with form:
@@ -185,9 +236,6 @@ def admin_page(*args, **kwargs):
                 st.selectbox(label="Name", options=lst_data)
                 st.form_submit_button(label="Remove Member")
 
-        def update_member_data(name):
-            pass
-
         with st.expander(label="Update Information"):
             with st.form(key="update_member"):
                 lst_data = get_data(column="Name", sheet="Main", worksheet=LST, range="A:K")
@@ -207,40 +255,9 @@ def account_page(name, username):
         st.text_input(label="Password")
         st.button(label="Save")
 
-def create_folder(name):
-    file_metadata = {
-        "name": name,
-        "mimeType": "application/vnd.google-apps.folder",
-        "parents": [FOLDER_ID],
-    }
-    file = drive_service.files().create(body=file_metadata, fields="id").execute()
-    return file
-
-def add_member(name, username, password, flags):
-    data = [name, username, password, flags]
-    # add user info to the Members sheet on LHT
-    add_entry(worksheet=LHT, sheet="Members", data=data, range="A:D")
-    body = {
-        "requests": [
-            {
-                "addSheet": {
-                    "properties": {
-                        "title": name,
-                    }
-                }
-            }
-        ]
-    }
-    # create a new tab with the users name
-    response = sheets_service.spreadsheets().batchUpdate(
-        spreadsheetId=USER_SHEET_ID,
-        body=body
-    ).execute()
-    # create folder on the drive with the users name
-    create_folder(name)
-
 def is_admin(name) -> bool:
     df = get_data(column=None, sheet="Members", worksheet=LHT)
+    # check if account has any flags
     output = df.query(f"Name == '{name}'")["Flags"]
     return True if list(output)[0] is not None else False
 
