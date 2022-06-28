c5eaaad9c3093757a8e22d43b0a0867d79c8cfa3
ticibi <tcb003@outlook.com>
2022-06-28 19:24:46 -0400

    update git ignore

--- ui_elements.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import base64
-
-def display_file(filename):
-    with open(f'temp/{filename}', "rb") as f:
-        pdf = base64.b64encode(f.read()).decode('utf-8')
-    html = f'<embed src="data:application/pdf;base64,{pdf}" width="700" height="900" type="application/pdf">'
-    st.markdown(html, unsafe_allow_html=True)
