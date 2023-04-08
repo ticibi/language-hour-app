3c158649ea6cd6c5bca97ba776ccf479c04a5b3a
ticibi <tcb003@outlook.com>
2023-04-08 16:50:29 -0400

    update add user form typing

---
+++ forms.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from models import User
+from db import session
+
+
+def add_user(db):
+    # Declare the form
+    with st.form('add_user'):
+        st.write('Add User')
+
+    # Create input fields
+    name = st.text_input('Name', placeholder='First M Last')
+    username = st.text_area('Username', placeholder='fmlast')
+    password = st.text_input('Password', type='password')
+    email = st.text_input('Email (optional)')
+    is_admin = st.checkbox('Admin')
+    group_id = st.number_input('Group ID',min_value=15, step=1)
+    if st.form_submit_button('Submit'):
+
+        # Create the user database model
+        user = User(
+            name=name,
+            username=username,
+            password=password,
+            email=email,
+            is_admin=bool(is_admin),
+            group_id=int(group_id),
+        )
+        
+        # Commit it to the database
+        with session(db) as db:
+            db.add(user)
+            st.success(f'Added {user.__name__}!')
+
