36ce4e2a609df795ddb3fa7fa9d86b5006b7ffc0
ticibi <tcb003@outlook.com>
2023-04-08 12:23:26 -0400

    update db session

--- db.py
+++ db.py
@@ -18,32 +18,31 @@ def session(db):
         db.close()
 
 def commit_or_rollback(db, commit: bool):
-    if commit:
-        db.commit()
-        db.refresh()
-        db.close()
-        st.success("Changes committed!")
-    else:
-        db.rollback()
-        st.warning("Changes rolled back.")
+    with session(db) as db:
+        if commit:
+            db.commit()
+            st.success("Changes committed!")
+        else:
+            db.rollback()
+            st.warning("Changes rolled back.")
 
 def get_user(db, username: str):
-        return db.query(User).filter(User.username == username).first()
+        with session(db) as db:
+            result = db.query(User).filter(User.username == username).first()
+        return result
 
 def reset_autoincrement(table_name):
     with engine.connect() as conn:
         conn.execute(f"ALTER TABLE {table_name} AUTO_INCREMENT = 1")
 
 def delete_row_by_id(db, cls, id):
-    row = db.query(cls).filter_by(id=id).first()
-    if row:
-        db.delete(row)
-        db.commit()
-        db.refresh()
-        db.close()
-        st.success(f"Row deleted successfully!")
-    else:
-        st.warning(f"Row not found.")
+    with session(db) as db:
+        row = db.query(cls).filter_by(id=id).first()
+        if row:
+            db.delete(row)
+            st.success(f"Row deleted successfully!")
+        else:
+            st.warning(f"Row not found.")
 
 @st.cache_resource
 def create_db():
