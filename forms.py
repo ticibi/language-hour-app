fd4bbd3cd9509dc85928e6ad9cda7abab0e2756a
ticibi <tcb003@outlook.com>
2023-04-08 16:57:20 -0400

    update

--- forms.py
+++ forms.py
@@ -8,27 +8,27 @@ def add_user(db):
     with st.form('add_user'):
         st.write('Add User')
 
-    # Create input fields
-    name = st.text_input('Name', placeholder='First M Last')
-    username = st.text_area('Username', placeholder='fmlast')
-    password = st.text_input('Password', type='password')
-    email = st.text_input('Email (optional)')
-    is_admin = st.checkbox('Admin')
-    group_id = st.number_input('Group ID',min_value=15, step=1)
-    if st.form_submit_button('Submit'):
+        # Create input fields
+        name = st.text_input('Name', placeholder='First M Last')
+        username = st.text_area('Username', placeholder='fmlast')
+        password = st.text_input('Password', type='password')
+        email = st.text_input('Email (optional)')
+        is_admin = st.checkbox('Admin')
+        group_id = st.number_input('Group ID',min_value=15, step=1)
+        if st.form_submit_button('Submit'):
 
-        # Create the user database model
-        user = User(
-            name=name,
-            username=username,
-            password=password,
-            email=email,
-            is_admin=bool(is_admin),
-            group_id=int(group_id),
-        )
-        
-        # Commit it to the database
-        with session(db) as db:
-            db.add(user)
-            st.success(f'Added {user.__name__}!')
+            # Create the user database model
+            user = User(
+                name=name,
+                username=username,
+                password=password,
+                email=email,
+                is_admin=bool(is_admin),
+                group_id=int(group_id),
+            )
+            
+            # Commit it to the database
+            with session(db) as db:
+                db.add(user)
+                st.success(f'Added {user.__name__}!')
 
